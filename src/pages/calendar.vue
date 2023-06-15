c7533018940780e5fb9b56e09a9406b38432e675
KAZAHv2 <91937513+KAZAHv2@users.noreply.github.com>
2023-06-15 16:46:39 +0300

    complate calendar

--- src/pages/calendar.vue
+++ src/pages/calendar.vue
@@ -6,6 +6,7 @@
     locale="uk"
     :time="false"
     active-view="month"
+    :events-on-month-view="[ 'short'][shortEventsOnMonthView * 1]"
   />
 </template>
 
@@ -14,7 +15,8 @@ import VueCal from 'vue-cal'
 import 'vue-cal/dist/vuecal.css'
 import {DataDB} from '@/stores/dataDB'
 const dataBase = DataDB()
-
+const showAllDayEvents = ref(0)
+const shortEventsOnMonthView = ref( false)
 onMounted( async () => {
   await allTask()
   console.log(tasks.value)
@@ -84,7 +86,23 @@ function sortArr(){
 .vuecal__cell--selected:before {border-color: #ccbcf8 !important;}
 .vuecal__event-title{ font-size: 20px; font-weight: bold; margin-top: 2%}
 .vuecal__event-content {margin-bottom: 2%}
-.vuecal__event { border-width: 6px; outline: 2px solid #000; margin-top: 2%}
+.vuecal__event {
+  border-width: 1px;
+  outline: 2px solid #000;
+  margin-top: 2%;
+  border-radius: 20px;
+  width: 90%;
+}
+.vuecal__cell-events{
+  margin-left: 5%;
+}
+.vuecal__event-title{
+  justify-content: center;
+  display: flex;
+}
+.vuecal__cell-date{
+  font-size: 20px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
